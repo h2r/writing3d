48ed5084915bd93fdbec22bb65321e86e1ab7e7c
Kaiyu Zheng <kaiyutony@gmail.com>
2019-01-01 11:15:47 -0500

    Added velocity_cmd

--- src/writing3d/movo_pose_publisher.py
+++ src/writing3d/movo_pose_publisher.py
@@ -5,19 +5,22 @@
 
 import rospy
 from std_msgs.msg import Header
-from movo_msgs.msg import JacoAngularVelocityCmd7DOF
+from movo_msgs.msg import JacoAngularVelocityCmd7DOF, JacoCartesianVelocityCmd
 
 SEQ = 1000
 
 def pose_publisher(msg):
-    pub = rospy.Publisher("movo/right_arm/angular_vel_cmd", JacoAngularVelocityCmd7DOF, queue_size=10)
+    if isinstance(msg, JacoCartesianVelocityCmd):
+        pub = rospy.Publisher("movo/right_arm/cartesian_vel_cmd", JacoCartesianVelocityCmd, queue_size=10)
+    else:
+        pub = rospy.Publisher("movo/right_arm/angular_vel_cmd", JacoAngularVelocityCmd7DOF, queue_size=10)
     rate = rospy.Rate(1)
     while not rospy.is_shutdown():
         rospy.loginfo("Publishing %s" % msg)
         pub.publish(msg)
         rate.sleep()
 
-def angular_pose(indices=[], new_vals=[]):
+def angular_vel(indices=[], new_vals=[]):
     global SEQ
     msg = JacoAngularVelocityCmd7DOF()
     msg.header = Header()
@@ -38,12 +41,37 @@ def angular_pose(indices=[], new_vals=[]):
     msg.theta_wrist_3_joint = vals[6]
     return msg
 
+def cartesian_vel(indices=[], new_vals=[]):
+    # cartesian velocity does not have joint-specific setting. For the entire arm,
+    # there are:
+    #
+    # x, y, z, theta_x, theta_y, theta_z
+    global SEQ
+    msg = JacoCartesianVelocityCmd()
+    msg.header = Header()
+    msg.header.stamp = rospy.Time.now()
+    msg.header.seq = SEQ; SEQ += 1
+
+    vals = [0] * 6
+    if len(indices) > 0 and len(new_vals) > 0 and len(indices) == len(new_vals):
+        for indx, i in enumerate(indices):
+            vals[i] = new_vals[indx]
+
+    msg.x = vals[0]
+    msg.y = vals[1]
+    msg.z = vals[2]
+    msg.theta_x = vals[3]
+    msg.theta_y = vals[4]
+    msg.theta_z = vals[5]
+    return msg
+
 
 if __name__ == "__main__":
     rospy.init_node("movo_pose_node", anonymous=True)
 
     try:
-        msg = angular_pose([3], [-0.4])
+        #msg = angular_vel([6], [2.0])
+        msg = cartesian_vel([0,1], [-0.1, 0.2])
         pose_publisher(msg)
     except rospy.ROSInterruptException:
         pass
